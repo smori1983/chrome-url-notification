d09cb7df9547bbc11bd27d0d5c9d5d22d5b13e36
smori1983 <shinichiro.mori.1983@gmail.com>
2016-05-28 11:24:24 +0900

    fix typo

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ var setUp = function() {
     urlNotifier.storage.deleteAll();
 };
 
-QUnit.module("number");
+QUnit.module("urlNotifier.storage");
 
 QUnit.test("delete all patterns", function(assert) {
     urlNotifier.storage.addPattern({ url: "http://example.com/", msg: "example.com" });
