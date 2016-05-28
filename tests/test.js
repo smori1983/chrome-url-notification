d74614cd36c2c3fd31db19086f85b24ed1b2d188
smori1983 <shinichiro.mori.1983@gmail.com>
2016-05-28 11:30:44 +0900

    [test] テストケース追加

--- tests/test.js
+++ tests/test.js
@@ -5,14 +5,30 @@ var setUp = function() {
 QUnit.module("urlNotifier.storage");
 
 QUnit.test("初期状態の件数", function(assert) {
+    setUp();
+
     assert.equal(0, urlNotifier.storage.getCount());
 });
 
 QUnit.test("全データ削除", function(assert) {
+    setUp();
+
     urlNotifier.storage.addPattern({ url: "http://example.com/1", msg: "example.com" });
     urlNotifier.storage.addPattern({ url: "http://example.com/2", msg: "example.com" });
 
     urlNotifier.storage.deleteAll();
 
     assert.equal(0, urlNotifier.storage.getCount());
+});
+
+QUnit.test("1件削除", function(assert) {
+    setUp();
+
+    urlNotifier.storage.addPattern({ url: "http://example.com/1", msg: "example.com" });
+    urlNotifier.storage.addPattern({ url: "http://example.com/2", msg: "example.com" });
+    urlNotifier.storage.addPattern({ url: "http://example.com/3", msg: "example.com" });
+
+    urlNotifier.storage.deletePattern({ url: "http://example.com/1", msg: "example.com" });
+
+    assert.equal(2, urlNotifier.storage.getCount());
 });
