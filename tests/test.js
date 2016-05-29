5e1cba34dfae830c750a34048ba47d110d0fa3c6
smori1983 <shinichiro.mori.1983@gmail.com>
2016-05-30 00:45:18 +0900

    [test] テストケース追加

--- tests/test.js
+++ tests/test.js
@@ -98,3 +98,15 @@ QUnit.test("URLで検索 該当データなし", function(assert) {
 
     assert.equal(null, result);
 });
+
+QUnit.test("URLで検索 *パターンにマッチ", function(assert) {
+    setUp();
+
+    urlNotifier.storage.addPattern({ url: "http://example.com/1", msg: "1" });
+    urlNotifier.storage.addPattern({ url: "http://example.com/2", msg: "2" });
+    urlNotifier.storage.addPattern({ url: "http://example.com/*", msg: "3" });
+
+    var result = urlNotifier.finder.find("http://example.com/3");
+
+    assert.equal(result.msg, "3");
+});
