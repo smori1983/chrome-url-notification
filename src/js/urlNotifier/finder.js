780de64ec0a61477083a7e2a665be35eb5de57f5
smori1983 <shinichiro.mori.1983@gmail.com>
2016-05-30 01:17:22 +0900

    URLパターン部分を正規表現文字列に置き換える処理を切り出し

--- src/js/urlNotifier/finder.js
+++ src/js/urlNotifier/finder.js
@@ -6,13 +6,7 @@ urlNotifier.finder = (function() {
         var result = null, i, len, allPattern = urlNotifier.storage.getAllPattern();
 
         for (i = 0, len = allPattern.length; i < len; i++) {
-            var str = allPattern[i].url.
-                replace(/\/|\.|\-|\+|\?/g, function(matched) {
-                    return "\\" + matched;
-                }).
-                replace(/\*/g, function(matched) {
-                    return "[0-9a-zA-Z\-_]+";
-                });
+            var str = _convertForMaching(allPattern[i].url);
 
             if (new RegExp(str).test(url)) {
                 result = allPattern[i];
@@ -21,6 +15,16 @@ urlNotifier.finder = (function() {
         }
 
         return result;
+    };
+
+    var _convertForMaching = function(url) {
+        return url.
+            replace(/\/|\.|\-|\+|\?/g, function(matched) {
+                return "\\" + matched;
+            }).
+            replace(/\*/g, function(matched) {
+                return "[0-9a-zA-Z\-_]+";
+            });
     };
 
     return {
