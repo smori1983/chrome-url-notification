5c348b0b1c3a99aa36fe97670333680afd81912e
smori1983 <shinichiro.mori.1983@gmail.com>
2017-03-20 02:20:39 +0900

    options.js リファクタリング

--- src/js/background.js
+++ src/js/background.js
@@ -4,12 +4,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var result;
 
     if ((result = urlNotifier.finder.find(request.url)) !== null) {
-
-        var style = {};
-        style.backgroundColor = "#" + result.backgroundColor;
-        style.fontColor = "#ffffff";
-
-        result.style = style;
+        result.style = {
+            backgroundColor: "#" + result.backgroundColor,
+            fontColor: "#ffffff"
+        };
 
         sendResponse(result);
     }
