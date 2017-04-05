16099412eb8825dc6dffc4dd454c44fb6d46f26c
smori1983 <shinichiro.mori.1983@gmail.com>
2017-04-06 02:08:21 +0900

    ビルドタスク追加
    - npm run build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require("gulp");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
 var qunit = require("node-qunit-phantomjs");
-var watch = require("watch");
+
+gulp.task("build", ["concat"]);
+
+gulp.task("concat", function(cb) {
+    pump([
+        gulp.src("./src/js/urlNotifier/*.js"),
+        concat("urlNotifier.js"),
+        gulp.dest("./src/js")
+    ], cb);
+});
 
 gulp.task("test", function() {
     qunit("./tests/test.html");
