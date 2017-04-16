5355b263c99a7df72a123575dbfaa5b092f06635
smori1983 <shinichiro.mori.1983@gmail.com>
2017-04-17 03:17:08 +0900

    urlNotifier.migration.migrateFrom() - 0 to 1

--- src/js/urlNotifier/migration.js
+++ src/js/urlNotifier/migration.js
@@ -2,7 +2,8 @@ var urlNotifier = urlNotifier || {};
 
 urlNotifier.migration = (function() {
     var key = {
-        version: "version"
+        version: "version",
+        pattern: "pattern"
     };
 
     var hasVersion = function() {
@@ -15,9 +16,64 @@ urlNotifier.migration = (function() {
         return /^\d+$/.test(version);
     };
 
+    var currentVersion = function() {
+        var version = localStorage.getItem(key.version);
+
+        if (version === null) {
+            return 0;
+        }
+
+        if (/^\d+$/.test(version)) {
+            return version;
+        }
+
+        return 0;
+    };
+
+    var shouldMigrate = function() {
+        return currentVersion() < urlNotifier.config.version();
+    };
+
+    var migrateFrom = function(currentVersion) {
+        if (migrationFunctions.hasOwnProperty(currentVersion)) {
+            migrationFunctions[currentVersion]();
+        }
+    };
+
+    var migrateFor0 = function() {
+        var result = [];
+        var data;
+
+        if ((data = localStorage.getItem(key.pattern)) !== null) {
+            $.each(JSON.parse(data), function(idx, item) {
+                if (typeof item.backgroundColor === "undefined") {
+                    item.backgroundColor = urlNotifier.config.defaultBackgroundColor();
+                }
+
+                result.push(item);
+            });
+        }
+
+        localStorage.setItem(key.pattern, JSON.stringify(result));
+        localStorage.setItem(key.version, 1);
+    };
+
+    var migrationFunctions = {
+        0: migrateFor0
+    };
+
     return {
         hasVersion: function() {
             return hasVersion();
+        },
+        currentVersion: function() {
+            return currentVersion();
+        },
+        shouldMigrate: function() {
+            return shouldMigrate();
+        },
+        migrateFrom: function(currentVersion) {
+            migrateFrom(currentVersion);
         }
     };
 })();
--- tests/version.js
+++ tests/version.js
@@ -1,3 +1,11 @@
+var getVersion = function() {
+    return localStorage.getItem("version");
+};
+
+var getPatterns = function() {
+    return JSON.parse(localStorage.getItem("pattern"));
+};
+
 QUnit.module("urlNotifier.migration", {
     beforeEach: function() {
         localStorage.clear();
@@ -27,4 +35,50 @@ QUnit.test("urlNotifier.migration.hasVersion - キーあり - 正常値", functi
     var result = urlNotifier.migration.hasVersion();
 
     assert.equal(result, true);
+});
+
+
+QUnit.module("urlNotifier.migration.0to1", {
+    beforeEach: function() {
+        localStorage.clear();
+    },
+    afterEach: function() {
+        localStorage.clear();
+    }
+});
+
+QUnit.test("urlNotifier.migration - version 0", function(assert) {
+    var result = urlNotifier.migration.currentVersion();
+
+    assert.equal(result, 0);
+});
+
+QUnit.test("urlNotifier.migration - version 0 to 1", function(assert) {
+    urlNotifier.storage.addPattern({ url: "http://example.com/1", msg: "1" });
+
+    var data = getPatterns();
+
+    var expected = [
+        { url: "http://example.com/1", msg: "1" }
+    ];
+
+    assert.propEqual(data, expected);
+
+    var shouldMigrate = urlNotifier.migration.shouldMigrate();
+
+    assert.equal(shouldMigrate, true);
+
+    urlNotifier.migration.migrateFrom(0);
+
+    var data = getPatterns();
+
+    var expected = [
+        { url: "http://example.com/1", msg: "1", backgroundColor: "000000" }
+    ];
+
+    assert.propEqual(data, expected);
+
+    var version = getVersion();
+
+    assert.equal(version, 1);
 });
