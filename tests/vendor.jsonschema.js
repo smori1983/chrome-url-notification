b685241544f211db86e76bd341fa78b4c39adc99
smori1983 <shinichiro.mori.1983@gmail.com>
2017-04-19 01:09:49 +0900

    [test] jsonschema インポートJSON テストケース

--- tests/vendor.jsonschema.js
+++ tests/vendor.jsonschema.js
@@ -1,21 +1,270 @@
 QUnit.module("vendor.jsonschema", {
     beforeEach: function() {
-        localStorage.clear();
+        this.validate = require("jsonschema").validate;
 
-        var Validator = require("jsonschema").Validator;
+        this.schema = {
+            "type": "object",
+            "properties": {
+                "version": {
+                    "required": true,
+                    "type": "integer"
+                },
+                "pattern": {
+                    "required": true,
+                    "type": "array",
+                    "items": {
+                        "properties": {
+                            "url": {
+                                "required": true,
+                                "type": "string"
+                            },
+                            "msg": {
+                                "required": true,
+                                "type": "string"
+                            },
+                            "backgroundColor": {
+                                "required": true,
+                                "type": "string"
+                            }
+                        }
+                    }
+                }
+            }
+        };
 
-        this.validator = new Validator();
+        this.isValid = function(json) {
+            return this.validate(json, this.schema).valid;
+        };
     },
     afterEach: function() {
-        localStorage.clear();
     }
 });
 
-QUnit.test("simple example", function(assert) {
-    var instance = 1;
-    var schema = { "type": "number" };
+QUnit.test("import json - error - array", function(assert) {
+    assert.notOk(this.isValid([]));
+});
 
-    var result = this.validator.validate(instance, schema);
+QUnit.test("import json - error - no keys", function(assert) {
+    assert.notOk(this.isValid({
+    }));
+});
 
-    assert.equal(result.valid, true);
+QUnit.test("import json - error - version not defined", function(assert) {
+    assert.notOk(this.isValid({
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern not defined", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1
+    }));
+});
+
+QUnit.test("import json - error - version is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: null,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - version is true", function(assert) {
+    assert.notOk(this.isValid({
+        version: true,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - version is false", function(assert) {
+    assert.notOk(this.isValid({
+        version: false,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - version is string of integer", function(assert) {
+    assert.notOk(this.isValid({
+        version: "1",
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - version is float", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1.1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: null
+    }));
+});
+
+QUnit.test("import json - error - pattern is true", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: true
+    }));
+});
+
+QUnit.test("import json - error - pattern is false", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: false
+    }));
+});
+
+QUnit.test("import json - error - pattern is string", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: "hoge"
+    }));
+});
+
+QUnit.test("import json - error - pattern is object", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: { url: "sample1", msg: "sample1", backgroundColor: "111111" }
+    }));
+});
+
+QUnit.test("import json - error - pattern - url is not defined", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            {                 msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - url is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: null,      msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - url is true", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: true,      msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - url is false", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: false,     msg: "sample2", backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - msg is not defined", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2",                 backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - msg is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: null,      backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - msg is true", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: true,      backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - msg is false", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: false,     backgroundColor: "222222" }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - backgroundColor is not defined", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2"                            }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - backgroundColor is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: null     }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - backgroundColor is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: true     }
+        ]
+    }));
+});
+
+QUnit.test("import json - error - pattern - backgroundColor is null", function(assert) {
+    assert.notOk(this.isValid({
+        version: 1,
+        pattern: [
+            { url: "sample1", msg: "sample1", backgroundColor: "111111" },
+            { url: "sample2", msg: "sample2", backgroundColor: false    }
+        ]
+    }));
 });
