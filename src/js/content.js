d272e17628932abb0799c13e169026a00afec14c
smori1983 <shinichiro.mori.1983@gmail.com>
2018-03-11 16:46:48 +0900

    update content.js #7
    - consider "displayPosition" value.

--- src/js/content.js
+++ src/js/content.js
@@ -9,26 +9,45 @@ chrome.runtime.sendMessage({
 
     var height = 50;
 
-    var container = $("<div>").css({
-        position:   "fixed",
-        top:        "0px",
-        left:       "0px",
-        width:      "100%",
-        height:     height + "px",
-        lineHeight: height + "px",
-        background: "#" + response.data.backgroundColor,
-        color:      "#" + response.data.fontColor,
-        fontSize:   "12px",
-        textAlign:  "center",
-        fontFamily: "Meiryo",
-        zIndex:     "1000",
+    var createCss = function() {
+        var result = {
+            position:   "fixed",
+            left:       "0px",
+            width:      "100%",
+            height:     height + "px",
+            lineHeight: height + "px",
+            background: "#" + response.data.backgroundColor,
+            color:      "#" + response.data.fontColor,
+            fontSize:   "12px",
+            textAlign:  "center",
+            fontFamily: "Meiryo",
+            zIndex:     "99999999",
 
-        webkitUserSelect: "none"
-    }).text(response.data.message);
+            webkitUserSelect: "none"
+        };
 
-    $("body").css({
-        marginTop: (height + $("body").offset().top) + "px"
-    }).append(container);
+        result[response.data.displayPosition] = "0px";
+
+        return result;
+    };
+
+    var container = $("<div>").
+        attr("id", "chrome-url-notification-message").
+        css(createCss()).
+        text(response.data.message);
+
+    if (response.data.displayPosition === "top") {
+        //
+        // workaround for "position: fixed;" page.
+        //
+        $("body").css({
+            marginTop: (height + $("body").offset().top) + "px"
+        }).append(container);
+    } else if (response.data.displayPosition === "bottom") {
+        $("body").css({
+            marginBottom: height + "px"
+        }).append(container);
+    }
 });
 
 });
