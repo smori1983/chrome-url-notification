e8de378cfc33ca576fb9b47e1cfc890f7e553356
smori1983 <shinichiro.mori.1983@gmail.com>
2018-03-17 20:38:48 +0900

    when importing json, migrate data in memory before store them #8

--- src/js/urlNotifier/importer.js
+++ src/js/urlNotifier/importer.js
@@ -23,6 +23,16 @@ urlNotifier.importer = (function() {
         2: prepareFor2,
     };
 
+    var migrate = function(pattern, version) {
+        var result = [];
+
+        pattern.forEach(function(item) {
+            result.push(urlNotifier.migrationExecuter.from(version, item));
+        });
+
+        return result;
+    };
+
     var addOrUpdate = function(data) {
         if (urlNotifier.storage.findByUrl(data.url)) {
             urlNotifier.storage.updatePattern(data.url, data);
@@ -32,16 +42,29 @@ urlNotifier.importer = (function() {
     };
 
     var importJson = function(json) {
-        if (prepares.hasOwnProperty(json.version)) {
-            json.pattern.forEach(function(item) {
-                addOrUpdate(prepares[json.version](item));
-            });
+        var version = json.version;
+        var pattern = json.pattern;
 
-            urlNotifier.background.migrate();
+        console.info("Import start.");
+
+        while (urlNotifier.config.version() > version) {
+            console.info("Migrate from scheme version " + version);
+
+            pattern = migrate(pattern, version);
+            version += 1;
         }
+
+        pattern.forEach(function(item) {
+            addOrUpdate(prepares[version](item));
+        });
+
+        console.info("Import done.");
     };
 
     return {
+        /**
+         * Assumes that json is validated.
+         */
         importJson: function(json) {
             importJson(json);
         }
