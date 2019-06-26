a087eb891a840ee4aa1660ac5909bb28fd570115
smori <shinichiro.mori.1983@gmail.com>
2019-06-26 21:10:24 +0900

    [test] Add tests for urlNotification.storage

--- test/storage.withoutData.js
+++ test/storage.withoutData.js
@@ -9,7 +9,13 @@ describe('urlNotification.storage.withoutData', function() {
     localStorage.clear();
   });
 
-  it('全件取得 - ローカルストレージにデータなし', function () {
+  it('全データ削除', function () {
+    urlNotification.storage.deleteAll();
+
+    assert.strictEqual(urlNotification.storage.getCount(), 0);
+  });
+
+  it('全件取得', function () {
     assert.strictEqual(urlNotification.storage.getAll().length, 0);
   });
 
@@ -17,10 +23,34 @@ describe('urlNotification.storage.withoutData', function() {
     assert.strictEqual(urlNotification.storage.getCount(), 0);
   });
 
+  it('URLで検索 該当データなし', function () {
+    assert.strictEqual(urlNotification.storage.findByUrl('http://example.com/'), null);
+  });
+
+  it('データ更新 - 該当データ無し', function () {
+    const item = {
+      url: 'http://example.com/',
+      msg: '!',
+      backgroundColor: '000000',
+      displayPosition: 'top',
+    };
+
+    urlNotification.storage.updatePattern('http://example.com/', item);
+
+    assert.strictEqual(urlNotification.storage.findByUrl('http://example.com/'), null);
+  });
+
   it('パターンの重複登録はできない', function () {
-    urlNotification.storage.addPattern({url: 'http://example.com/1', msg: '1'});
-    urlNotification.storage.addPattern({url: 'http://example.com/1', msg: '1'});
-    urlNotification.storage.addPattern({url: 'http://example.com/1', msg: '1'});
+    const item = {
+      url: 'http://example.com/1',
+      msg: '1',
+      backgroundColor: '000000',
+      displayPosition: 'top',
+    };
+
+    urlNotification.storage.addPattern(item);
+    urlNotification.storage.addPattern(item);
+    urlNotification.storage.addPattern(item);
 
     assert.strictEqual(urlNotification.storage.getCount(), 1);
   });
