46cac29e77f987154664752d04e3f80dd7500913
smori <shinichiro.mori.1983@gmail.com>
2019-06-26 23:03:29 +0900

    [test] Refactor and update tests for migration

--- test/migration.js
+++ test/migration.js
@@ -9,97 +9,96 @@ describe('urlNotification.migration', function() {
     localStorage.clear();
   });
 
-  it('urlNotification.migration.hasVersion - キーなし', function () {
-    const result = urlNotification.migration.hasVersion();
+  describe('version', function () {
+    it('キーなし', function () {
+      assert.strictEqual(urlNotification.migration.hasVersion(), false);
+      assert.strictEqual(urlNotification.migration.currentVersion(), 0);
+    });
 
-    assert.strictEqual(result, false);
+    it('キーあり - 不正値 - 小数', function () {
+      localStorage.setItem('version', '1.1');
+
+      assert.strictEqual(urlNotification.migration.hasVersion(), false);
+      assert.strictEqual(urlNotification.migration.currentVersion(), 0);
+    });
+
+    it('urlNotification.migration.hasVersion - キーあり - 不正値 - 文字列', function () {
+      localStorage.setItem('version', 'foo');
+
+      assert.strictEqual(urlNotification.migration.hasVersion(), false);
+      assert.strictEqual(urlNotification.migration.currentVersion(), 0);
+    });
+
+    it('urlNotification.migration.hasVersion - キーあり - 正常値', function () {
+      localStorage.setItem('version', '1');
+
+      assert.strictEqual(urlNotification.migration.hasVersion(), true);
+      assert.strictEqual(urlNotification.migration.currentVersion(), 1);
+    });
   });
 
-  it('urlNotification.migration.hasVersion - キーあり - 不正値 - 小数', function () {
-    localStorage.setItem('version', 1.1);
+  describe('0to1', function () {
+    beforeEach(function () {
+      localStorage.setItem('pattern', JSON.stringify([
+        { url: 'http://example.com/1', msg: '1' },
+      ]));
+    });
 
-    const result = urlNotification.migration.hasVersion();
+    it('current version is 0', function () {
+      assert.strictEqual(urlNotification.migration.currentVersion(), 0);
+    });
 
-    assert.strictEqual(result, false);
+    it('execute migration', function () {
+      const expectedBefore = [
+        { url: 'http://example.com/1', msg: '1' },
+      ];
+
+      assert.deepStrictEqual(urlNotification.storage.getAll(), expectedBefore);
+
+      assert.strictEqual(urlNotification.migration.shouldMigrate(), true);
+
+      urlNotification.migration.migrateFrom(0);
+
+      const expectedAfter = [
+        { url: 'http://example.com/1', msg: '1', backgroundColor: '000000' },
+      ];
+
+      assert.deepStrictEqual(urlNotification.storage.getAll(), expectedAfter);
+
+      assert.strictEqual(urlNotification.migration.currentVersion(), 1);
+    });
   });
 
-  it('urlNotification.migration.hasVersion - キーあり - 不正値 - 文字列', function () {
-    localStorage.setItem('version', 'foo');
+  describe('1to2', function() {
+    beforeEach(function() {
+      localStorage.setItem('version', '1');
+      localStorage.setItem('pattern', JSON.stringify([
+        { url: 'http://example.com/1', msg: '1', backgroundColor: '000000' },
+      ]));
+    });
 
-    const result = urlNotification.migration.hasVersion();
+    it('current version is 1', function() {
+      assert.strictEqual(urlNotification.migration.currentVersion(), 1);
+    });
 
-    assert.strictEqual(result, false);
-  });
+    it('execute migration', function() {
+      const expectedBefore = [
+        { url: 'http://example.com/1', msg: '1', backgroundColor: '000000' },
+      ];
 
-  it('urlNotification.migration.hasVersion - キーあり - 正常値', function () {
-    localStorage.setItem('version', 1);
+      assert.deepStrictEqual(urlNotification.storage.getAll(), expectedBefore);
 
-    const result = urlNotification.migration.hasVersion();
+      assert.strictEqual(urlNotification.migration.shouldMigrate(), true);
 
-    assert.strictEqual(result, true);
-  });
+      urlNotification.migration.migrateFrom(1);
 
-  it('urlNotification.migration.currentVersion - キーなし', function () {
-    const result = urlNotification.migration.currentVersion();
+      const expectedAfter = [
+        { url: 'http://example.com/1', msg: '1', backgroundColor: '000000', displayPosition: 'top' },
+      ];
 
-    assert.strictEqual(result, 0);
-  });
+      assert.deepStrictEqual(urlNotification.storage.getAll(), expectedAfter);
 
-  it('urlNotification.migration.currentVersion - キーあり - 不正値 - 小数', function () {
-    localStorage.setItem('version', 1.1);
-
-    const result = urlNotification.migration.currentVersion();
-
-    assert.strictEqual(result, 0);
-  });
-
-  it('urlNotification.migration.currentVersion - キーあり - 不正値 - 文字列', function () {
-    localStorage.setItem('version', 'foo');
-
-    const result = urlNotification.migration.currentVersion();
-
-    assert.strictEqual(result, 0);
-  });
-
-  it('urlNotification.migration.currentVersion - キーあり - 正常値', function () {
-    localStorage.setItem('version', 1);
-
-    const result = urlNotification.migration.currentVersion();
-
-    assert.strictEqual(result, 1);
-  });
-});
-
-describe('urlNotification.migration.0to1', function() {
-  beforeEach(function () {
-    localStorage.clear();
-
-    urlNotification.storage.addPattern({url: 'http://example.com/1', msg: '1'});
-  });
-
-  it('urlNotification.migration - version 0', function () {
-    const result = urlNotification.migration.currentVersion();
-
-    assert.strictEqual(result, 0);
-  });
-
-  it('urlNotification.migration - version 0 to 1', function () {
-    const expectedBefore = [
-      {url: 'http://example.com/1', msg: '1'},
-    ];
-
-    assert.deepStrictEqual(urlNotification.storage.getAll(), expectedBefore);
-
-    assert.strictEqual(urlNotification.migration.shouldMigrate(), true);
-
-    urlNotification.migration.migrateFrom(0);
-
-    const expectedAfter = [
-      {url: 'http://example.com/1', msg: '1', backgroundColor: '000000'},
-    ];
-
-    assert.deepStrictEqual(urlNotification.storage.getAll(), expectedAfter);
-
-    assert.strictEqual(urlNotification.migration.currentVersion(), 1);
+      assert.strictEqual(urlNotification.migration.currentVersion(), 2);
+    });
   });
 });
