218f03a0d5b25af9f7e9dd01f2d6d9e9184095b8
smori <shinichiro.mori.1983@gmail.com>
2019-06-26 21:09:04 +0900

    [test] Add tests for migration

--- test/background.migrate.from.1.js
+++ test/background.migrate.from.1.js
@@ -4,24 +4,38 @@ const it = require('mocha').it;
 const assert = require('assert');
 const urlNotification = require('../src/js/urlNotification/main');
 
+const expectedVersion = 2;
+
 describe('urlNotification.background.migrate.from.1', function() {
   beforeEach(function() {
     localStorage.clear();
+  });
 
-    urlNotification.storage.addPattern({ url: 'http://example.com/1', msg: '1', backgroundColor: '111111' });
-    urlNotification.storage.addPattern({ url: 'http://example.com/2', msg: '2', backgroundColor: '222222' });
+  it('no data', function() {
+    urlNotification.background.migrate();
+
+    const expected = [];
+
+    assert.deepStrictEqual(urlNotification.storage.getAll(), expected);
+
+    assert.strictEqual(urlNotification.migration.currentVersion(), expectedVersion);
   });
 
   it('migrate', function() {
+    urlNotification.storage.addPattern({ url: 'http://example.com/1', msg: '1', backgroundColor: '111111' });
+    urlNotification.storage.addPattern({ url: 'http://example.com/2', msg: '2', backgroundColor: '222222' });
+    urlNotification.storage.addPattern({ url: 'http://example.com/3', msg: '3' });
+
     urlNotification.background.migrate();
 
     const expected = [
       { url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top' },
       { url: 'http://example.com/2', msg: '2', backgroundColor: '222222', displayPosition: 'top' },
+      { url: 'http://example.com/3', msg: '3', backgroundColor: '000000', displayPosition: 'top' },
     ];
 
     assert.deepStrictEqual(urlNotification.storage.getAll(), expected);
 
-    assert.strictEqual(urlNotification.migration.currentVersion(), 2);
+    assert.strictEqual(urlNotification.migration.currentVersion(), expectedVersion);
   });
 });
