62b2c5eb28e16c0850ddf6b31202b0101a197e5b
smori <shinichiro.mori.1983@gmail.com>
2019-09-12 00:00:39 +0900

    Add uiController to encapsulate jQuery code

--- src/js/app/main.content.js
+++ src/js/app/main.content.js
@@ -1,18 +1,6 @@
 'use strict';
 
-const cssFactory = require('./css');
-const $ = require('jquery');
-
-const messageContainerId = 'chrome-url-notification-container-9b7414d403c1287ca963';
-
-const $body = $('body');
-
-const css = cssFactory.init({
-  body: {
-    marginTop: $body.css('marginTop'),
-    marginBottom: $body.css('marginBottom'),
-  },
-});
+const uiController = require('./uiController').init();
 
 /**
  * @param {string} url
@@ -35,8 +23,8 @@ const process = function(response) {
     return;
   }
 
-  initUI(response.data);
-  updateUI(response.data);
+  uiController.initUI(response.data);
+  uiController.updateUI(response.data);
 };
 
 chrome.runtime.sendMessage(createRequest(location.href), process);
@@ -53,37 +41,11 @@ const tabNotifyStatusListener = function(request) {
   const item = request.data.item;
 
   if (item) {
-    updateUI(item);
+    uiController.updateUI(item);
   }
 };
 
 chrome.runtime.onMessage.addListener(tabNotifyStatusListener);
-
-/**
- * @param {FoundItem} item
- */
-const initUI = function(item) {
-  $('<div>')
-    .attr('id', messageContainerId)
-    .css(css.forMessage(item))
-    .text(item.message)
-    .appendTo($body);
-};
-
-/**
- * @param {PatternItem|FoundItem} item
- */
-const updateUI = function(item) {
-  const selector = '#' + messageContainerId;
-
-  if (item.status === 1) {
-    $(selector).show();
-  } else {
-    $(selector).hide();
-  }
-
-  $body.css(css.forBody(item.displayPosition, item.status));
-};
 
 /**
  * @typedef {Object} TabsRequest
