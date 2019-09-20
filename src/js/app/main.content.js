794033d4ad371541109e6f7b95bcd26839ccfa3d
smori <shinichiro.mori.1983@gmail.com>
2019-09-20 23:14:40 +0900

    content.tab.js

--- src/js/app/main.content.js
+++ src/js/app/main.content.js
@@ -1,27 +1,7 @@
 'use strict';
 
 const contentFind = require('./content.find');
-const uiController = require('./uiController').init();
+const contentTab = require('./content.tab');
 
 contentFind.sendMessage();
-
-/**
- * @typedef {Object} MessageTabNotifyStatus
- * @property {string} command
- * @property {{item: PatternItem|null, status: number}} data
- */
-
-/**
- * @param {MessageTabNotifyStatus} request
- */
-const tabNotifyStatusListener = function(request) {
-  if (request.command !== 'tab:notify:status') {
-    return;
-  }
-
-  if (request.data.item) {
-    uiController.updateUI(request.data.item);
-  }
-};
-
-chrome.runtime.onMessage.addListener(tabNotifyStatusListener);
+contentTab.listen();
---
+++ test/app/message.content.tab.js
@@ -0,0 +1,96 @@
+const describe = require('mocha').describe;
+const before = require('mocha').before;
+const beforeEach = require('mocha').beforeEach;
+const after = require('mocha').after;
+const afterEach = require('mocha').afterEach;
+const it = require('mocha').it;
+const assert = require('assert');
+const chrome = require('sinon-chrome');
+const JSDOM = require('jsdom').JSDOM;
+const SUT = require('../../src/js/app/content.tab');
+
+const html = `
+<html>
+<body>
+  <div id="chrome-url-notification-container-9b7414d403c1287ca963"></div>
+</body>
+</html>
+`;
+
+describe('message.content.tab', function () {
+  before(function() {
+    global.chrome = chrome;
+  });
+
+  beforeEach(function () {
+    chrome.flush();
+    delete require.cache[require.resolve('jquery')];
+  });
+
+  afterEach(function () {
+
+  });
+
+  after(function() {
+    delete(global.chrome);
+  });
+
+  it('pattern matched and status is 0', function () {
+    const dom = new JSDOM(html);
+
+    global.window = dom.window;
+    global.document = dom.window;
+
+    const $ = require('jquery');
+
+    SUT.listen();
+
+    chrome.runtime.onMessage
+      .dispatch({
+        command: 'tab:notify:status',
+        data: {
+          item: {
+            url: 'https://example.com/',
+            msg: 'message',
+            backgroundColor: '000000',
+            displayPosition: 'top',
+            status: 0,
+          },
+          status: 0,
+        },
+      });
+
+    // NOTE: default margin-top of <body> is 8px
+    assert.strictEqual($('body').css('margin-top'), '8px');
+    assert.strictEqual($('div').css('display'), 'none');
+  });
+
+  it('pattern matched and status is 1', function () {
+    const dom = new JSDOM(html);
+
+    global.window = dom.window;
+    global.document = dom.window;
+
+    const $ = require('jquery');
+
+    SUT.listen();
+
+    chrome.runtime.onMessage
+      .dispatch({
+        command: 'tab:notify:status',
+        data: {
+          item: {
+            url: 'https://example.com/',
+            msg: 'message',
+            backgroundColor: '000000',
+            displayPosition: 'bottom',
+            status: 1,
+          },
+          status: 1,
+        },
+      });
+
+    assert.strictEqual($('body').css('margin-bottom'), '50px');
+    assert.strictEqual($('div').css('display'), 'block');
+  });
+});
