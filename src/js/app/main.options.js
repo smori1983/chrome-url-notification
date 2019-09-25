f2193189b8c3518516a3261f3c243290aa54b141
smori <shinichiro.mori.1983@gmail.com>
2019-09-26 00:31:38 +0900

    i18n.appy() to 'body'

--- src/js/app/main.options.js
+++ src/js/app/main.options.js
@@ -5,39 +5,11 @@ require('jquery-validation');
 require('bootstrap');
 require('bootstrap-colorpicker');
 
+const i18n = require('./i18n');
 const exportForm = require('./options.exportForm');
 const importForm = require('./options.importForm');
 const patternList = require('./options.list');
 const patternForm = require('./options.patternForm');
-
-const i18n = (function() {
-  const init = function() {
-    $('*[data-i18n]').each(function () {
-      $(this).text(get($(this).data('i18n')));
-    });
-
-    $('*[data-i18n-val]').each(function () {
-      $(this).val(get($(this).data('i18n-val')));
-    });
-
-    $('*[data-i18n-ph]').each(function () {
-      $(this).attr('placeholder', get($(this).data('i18n-ph')));
-    });
-  };
-
-  /**
-   * @param {string} key
-   * @returns {string}
-   */
-  const get = function(key) {
-    return chrome.i18n.getMessage(key);
-  };
-
-  return {
-    init: init,
-    get: get,
-  };
-})();
 
 const headerComponent = (function() {
   const showVersion = function() {
@@ -77,5 +49,5 @@ $(function() {
   headerComponent.init();
   patternList.show();
 
-  i18n.init();
+  i18n.apply('body');
 });
