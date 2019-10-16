09a9a05ac4e3362a3bf4c8cf204d3cb3d4f09681
smori <shinichiro.mori.1983@gmail.com>
2019-10-16 23:58:54 +0900

    Add popup.block.matched.js

--- src/js/app/main.popup.js
+++ src/js/app/main.popup.js
@@ -1,20 +1,5 @@
-const popupFind = require('./popup.find');
-const $ = require('jquery');
-
 const blockAll = require('./popup.block.all');
-
-$(function () {
-  $('#i18n_label_status').text(chrome.i18n.getMessage('label_status'));
-  $('#i18n_label_enabled').text(chrome.i18n.getMessage('label_enabled'));
-});
-
-$(function () {
-  chrome.tabs.query({
-    currentWindow: true,
-    active: true,
-  }, function(/** @type {chrome.tabs.Tab[]} */ tabs) {
-    popupFind.sendMessage(tabs[0]);
-  });
-});
+const blockMatched = require('./popup.block.matched');
 
 blockAll.show();
+blockMatched.show();
