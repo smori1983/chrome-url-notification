fe234f29786e6a536a46b300e33783f2ab436f52
smori <shinichiro.mori.1983@gmail.com>
2019-10-19 00:16:28 +0900

    [test] Refactor popup.block.matched.js

--- test/app/popup.block.matched.js
+++ test/app/popup.block.matched.js
@@ -4,6 +4,40 @@ const chrome = require('sinon-chrome');
 const SUT = require('../../src/js/app/popup.block.matched');
 const testUtil = require('../../test_lib/util');
 
+/**
+ * @param {string} url used to part of chrome.tabs.Tab
+ */
+const popupTabsQuery = function (url) {
+  chrome.tabs.query
+    .withArgs({
+      currentWindow: true,
+      active: true,
+    })
+    .callArgWith(1, [{
+      url: url,
+    }]);
+};
+
+/**
+ * NOTE: This function is copied from message.popup.find.js
+ *
+ * @param {chrome.tabs.Tab} tab
+ * @param {(FoundItem|null)} item
+ */
+const popupFindMessage = function(tab, item) {
+  chrome.runtime.sendMessage
+    .withArgs({
+      command: 'browser_action:find',
+      data: {
+        url: tab.url,
+      },
+    })
+    .callArgWith(1, {
+      matched: item !== null,
+      data: item,
+    });
+};
+
 describe('popup.block.matched', function () {
   before(testUtil.uiBase.before);
   beforeEach(testUtil.uiBase.beforeEach);
@@ -32,29 +66,14 @@ describe('popup.block.matched', function () {
 
   describe('initial state of status checkbox', function () {
     it('pattern matched and status is 0', function () {
-      chrome.tabs.query
-        .withArgs({
-          currentWindow: true,
-          active: true,
-        })
-        .callArgWith(1, [{
-          url: 'https://foo.example.com/page',
-        }]);
-
-      chrome.runtime.sendMessage
-        .withArgs({
-          command: 'browser_action:find',
-          data: {
-            url: 'https://foo.example.com/page',
-          },
-        })
-        .callArgWith(1, {
-          matched: true,
-          data: {
-            url: 'foo.example.com',
-            status: 0,
-          },
-        });
+      popupTabsQuery('https://foo.example.com/page');
+      popupFindMessage({
+        id: 10001,
+        url: 'https://foo.example.com/page',
+      }, testUtil.makeFoundItem({
+        url: 'foo.example.com',
+        status: 0,
+      }));
 
       const $ = require('jquery');
       const $element = $('#pattern_status');
@@ -63,29 +82,14 @@ describe('popup.block.matched', function () {
     });
 
     it('pattern matched and status is 1', function () {
-      chrome.tabs.query
-        .withArgs({
-          currentWindow: true,
-          active: true,
-        })
-        .callArgWith(1, [{
-          url: 'https://foo.example.com/page',
-        }]);
-
-      chrome.runtime.sendMessage
-        .withArgs({
-          command: 'browser_action:find',
-          data: {
-            url: 'https://foo.example.com/page',
-          },
-        })
-        .callArgWith(1, {
-          matched: true,
-          data: {
-            url: 'foo.example.com',
-            status: 1,
-          },
-        });
+      popupTabsQuery('https://foo.example.com/page');
+      popupFindMessage({
+        id: 10002,
+        url: 'https://foo.example.com/page',
+      }, testUtil.makeFoundItem({
+        url: 'foo.example.com',
+        status: 1,
+      }));
 
       const $ = require('jquery');
       const $element = $('#pattern_status');
