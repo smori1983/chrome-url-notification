ee08391ae933ff0d62bca8bd997cfb196c6476a9
smori <shinichiro.mori.1983@gmail.com>
2020-04-27 19:03:40 +0900

    [test] Shared assertions for migrate

--- test/urlNotification/background.migrate.from.2.js
+++ test/urlNotification/background.migrate.from.2.js
@@ -2,27 +2,20 @@ const { describe, beforeEach, it } = require('mocha');
 const assert = require('assert');
 const testUtil = require('../../test_lib/util');
 const SUT = require('../../src/js/urlNotification/background');
-const migration = require('../../src/js/urlNotification/migration');
 const storage = require('../../src/js/urlNotification/storage');
+const sharedMigrate = require('./shared/migrate');
 
 describe('urlNotification.background.migrate.from.2', function() {
   describe('no data', function() {
-    it('migrate', function () {
-      testUtil.setUpStorage('2', []);
-
-      SUT.migrate();
-
-      assert.deepStrictEqual(storage.getAll(), []);
-      assert.strictEqual(migration.currentVersion(), testUtil.currentVersion());
-    });
+    sharedMigrate.runNoData('2');
   });
 
   describe('with data', function() {
     beforeEach(function () {
       testUtil.setUpStorage('2', [
-        { url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top' },
-        { url: 'http://example.com/2', msg: '2', backgroundColor: '222222', displayPosition: 'bottom' },
-        { url: 'http://example.com/3', msg: '3', backgroundColor: '333333', displayPosition: 'top' },
+        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top'},
+        {url: 'http://example.com/2', msg: '2', backgroundColor: '222222', displayPosition: 'bottom'},
+        {url: 'http://example.com/3', msg: '3', backgroundColor: '333333', displayPosition: 'top'},
       ]);
     });
 
@@ -30,14 +23,13 @@ describe('urlNotification.background.migrate.from.2', function() {
       SUT.migrate();
 
       const expected = [
-        { url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top',    status: 1 },
-        { url: 'http://example.com/2', msg: '2', backgroundColor: '222222', displayPosition: 'bottom', status: 1 },
-        { url: 'http://example.com/3', msg: '3', backgroundColor: '333333', displayPosition: 'top',    status: 1 },
+        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top',    status: 1},
+        {url: 'http://example.com/2', msg: '2', backgroundColor: '222222', displayPosition: 'bottom', status: 1},
+        {url: 'http://example.com/3', msg: '3', backgroundColor: '333333', displayPosition: 'top',    status: 1},
       ];
 
       assert.deepStrictEqual(storage.getAll(), expected);
-
-      assert.strictEqual(migration.currentVersion(), testUtil.currentVersion());
+      assert.strictEqual(storage.currentVersion(), testUtil.currentVersion());
     });
   });
 });
---
+++ test/urlNotification/shared/migrate.js
@@ -0,0 +1,19 @@
+const { it } = require('mocha');
+const assert = require('assert');
+const testUtil = require('../../../test_lib/util');
+const background = require('../../../src/js/urlNotification/background');
+const storage = require('../../../src/js/urlNotification/storage');
+
+/**
+ * @param {string} version
+ */
+module.exports.runNoData = function (version) {
+  it('migrate', function () {
+    testUtil.setUpStorage(version, []);
+
+    background.migrate();
+
+    assert.deepStrictEqual(storage.getAll(), []);
+    assert.strictEqual(storage.currentVersion(), testUtil.currentVersion());
+  });
+};
