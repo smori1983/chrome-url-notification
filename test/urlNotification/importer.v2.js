32b079f946606ed3cda1f2c5fbf682c45eed8187
smori <shinichiro.mori.1983@gmail.com>
2020-04-30 22:35:55 +0900

    [test] Update importer tests

--- test/urlNotification/importer.v2.js
+++ test/urlNotification/importer.v2.js
@@ -1,4 +1,5 @@
-const { describe, it } = require('mocha');
+const { describe } = require('mocha');
+const { given } = require('mocha-testdata');
 const assert = require('assert');
 const testUtil = require('../../test_lib/util');
 const SUT = require('../../src/js/urlNotification/importer');
@@ -6,48 +7,40 @@ const storage = require('../../src/js/urlNotification/storage');
 
 describe('urlNotification.importer.v2', function() {
   describe('import v2 and migrate to v4', function () {
-    it('without existing data - case1', function () {
+    given([
+      {
+        item: {url: 'example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top'},
+        to:   {url: 'example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top', status: 1},
+      },
+      {
+        item: {url: 'example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'bottom'},
+        to:   {url: 'example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'bottom', status: 1},
+      },
+    ]).it('without existing data', function (arg) {
       testUtil.clearStorage();
 
-      SUT.importJson({version: 2, pattern: [
-        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top'},
-      ]});
+      SUT.importJson({version: 2, pattern: [arg.item]});
 
-      const expected = [
-        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top', status: 1},
-      ];
-
-      assert.deepStrictEqual(storage.getAll(), expected);
+      assert.deepStrictEqual(storage.getAll(), [arg.to]);
     });
 
-    it('without existing data - case2', function () {
-      testUtil.clearStorage();
+    given([
+      {
+        from: {url: 'example.com/1', msg: '1', backgroundColor: 'ffffff', displayPosition: 'top'},
+        item: {url: 'example.com/1', msg: '2', backgroundColor: 'ffffff', displayPosition: 'top'},
+        to:   {url: 'example.com/1', msg: '2', backgroundColor: 'ffffff', displayPosition: 'top', status: 1},
+      },
+      {
+        from: {url: 'example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top'},
+        item: {url: 'example.com/1', msg: '2', backgroundColor: '222222', displayPosition: 'bottom'},
+        to:   {url: 'example.com/1', msg: '2', backgroundColor: '222222', displayPosition: 'bottom', status: 1},
+      },
+    ]).it('with existing data', function (arg) {
+      testUtil.setUpStorage('2', [arg.from]);
 
-      SUT.importJson({version: 2, pattern: [
-        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'bottom'},
-      ]});
+      SUT.importJson({ version: 2, pattern: [arg.item]});
 
-      const expected = [
-        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'bottom', status: 1},
-      ];
-
-      assert.deepStrictEqual(storage.getAll(), expected);
-    });
-
-    it('with existing data', function () {
-      testUtil.setUpStorage('2', [
-        {url: 'http://example.com/1', msg: '1', backgroundColor: '111111', displayPosition: 'top'},
-      ]);
-
-      SUT.importJson({ version: 2, pattern: [
-        {url: 'http://example.com/1', msg: '1-edit', backgroundColor: '222222', displayPosition: 'bottom'},
-      ]});
-
-      const expected = [
-        {url: 'http://example.com/1', msg: '1-edit', backgroundColor: '222222', displayPosition: 'bottom', status: 1},
-      ];
-
-      assert.deepStrictEqual(storage.getAll(), expected);
+      assert.deepStrictEqual(storage.getAll(), [arg.to]);
     });
   });
 });
