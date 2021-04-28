4ff713dd4f78339a7d9e36ef513991fe4634d127
smori <shinichiro.mori.1983@gmail.com>
2021-04-28 22:34:32 +0900

    Use manifest.json instead of package.json to get version information

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
-const packageJson = require('./package.json');
+const fs = require('fs');
+const manifest = JSON.parse(fs.readFileSync('src/manifest.json').toString());
+
 const webpack = require('./webpack');
 const mode = 'production';
 const imageDir = 'image';
-const outputPath = 'dist/chrome-url-notification-v' + packageJson.version;
+const outputPath = 'dist/chrome-url-notification-v' + manifest.version;
 
 module.exports = webpack.prepare(mode, imageDir, outputPath);
