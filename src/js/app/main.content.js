2a578de7596166e3ddc26053b1193d2ecec2af04
smori <shinichiro.mori.1983@gmail.com>
2021-07-31 13:37:40 +0900

    Rewrite content.pageInfo.js usage code

--- src/js/app/main.content.js
+++ src/js/app/main.content.js
@@ -1,6 +1,8 @@
-const pageInfo = require('./content.pageInfo').init();
+const pageInfoFactory = require('./content.pageInfo');
 const contentFind = require('./content.find');
 const contentTab = require('./content.tab');
+
+const pageInfo = pageInfoFactory.init();
 
 contentFind.sendMessage(pageInfo.get());
 contentTab.listen(pageInfo.get());
