6a0d75b2db1deb9ac75d935ea1cfd2affb8b8d14
smori <shinichiro.mori.1983@gmail.com>
2021-10-28 01:19:00 +0900

    Move popup.block.all.js code to popup.ui.js

--- src/js/app/popup.ui.js
+++ src/js/app/popup.ui.js
@@ -1,8 +1,22 @@
-const blockAll = require('./popup.block.all');
 const blockMatched = require('./popup.block.matched');
 
+const showCommonMenu = () => {
+  const $ = require('jquery');
+
+  $('<a>')
+    .attr('href', '#')
+    .text(chrome.i18n.getMessage('label_options'))
+    .on('click', (e) => {
+      e.preventDefault();
+      chrome.tabs.create({
+        url: chrome.runtime.getURL('html/options.html'),
+      });
+    })
+    .appendTo($('#link_options'));
+};
+
 const init = () => {
-  blockAll.showCommonMenu();
+  showCommonMenu();
   blockMatched.showMatchedMenu();
 };
 
---
+++ test/app/popup.ui.js
@@ -0,0 +1,39 @@
+const { describe, before, beforeEach, it } = require('mocha');
+const assert = require('assert');
+const chrome = require('sinon-chrome');
+const SUT = require('../../src/js/app/popup.ui');
+const testUtil = require('../../test_lib/util');
+
+describe('app.popup.ui', () => {
+  describe('common menu', () => {
+    before(testUtil.uiBase.initI18n('en'));
+    testUtil.uiBase.registerHooks();
+    beforeEach(() => {
+      testUtil.uiBase.initDom(testUtil.getHtml('src/html/popup.html'));
+      SUT.init();
+    });
+
+    it('link to options page should be shown', () => {
+      const $ = require('jquery');
+      const $link = $('#link_options a');
+
+      assert.strictEqual($link.length, 1);
+      assert.strictEqual($link.eq(0).text(), 'Options');
+    });
+
+    it('click link to options page', () => {
+      const path = 'chrome-extension://xxx/html/options.html';
+
+      chrome.runtime.getURL
+        .withArgs('html/options.html')
+        .returns(path);
+
+      const $ = require('jquery');
+      const $link = $('#link_options a').eq(0);
+
+      $link.trigger('click');
+
+      assert.ok(testUtil.chrome.tabsCreateCalledWith(path));
+    });
+  });
+});
