813421cb61156aaf1e5c85fb6cb650424038a358
smori <shinichiro.mori.1983@gmail.com>
2021-11-04 00:11:17 +0900

    [test] joi - string.existingUrl

--- test/vendor/joi.js
+++ test/vendor/joi.js
@@ -102,5 +102,72 @@ describe('vendor.joi', () => {
       assert.deepStrictEqual(result.error.details[1].path, ['value3']);
       assert.deepStrictEqual(result.error.details[1].type, 'string.in');
     });
+
+    it('string.existingUrl', () => {
+      /**
+       * @type {Joi.Root}
+       */
+      const custom = Joi.extend({
+        type: 'string',
+        rules: {
+          existingUrl: {
+            args: ['mode', 'originalUrl'],
+            method(mode, originalUrl) {
+              return this.$_addRule({
+                name: 'existingUrl',
+                args: {mode, originalUrl},
+              });
+            },
+            validate(value, helpers, { mode, originalUrl }) {
+              // example code for testing.
+              const registered = ['a.example.com', 'z.example.com'];
+
+              if (mode === 'add') {
+                if (registered.indexOf(value) >= 0) {
+                  return helpers.error('string.existingUrl');
+                }
+              }
+
+              if (mode === 'edit') {
+                if (value !== originalUrl && registered.indexOf(value) >= 0) {
+                  return helpers.error('string.existingUrl');
+                }
+              }
+            },
+          },
+        },
+        messages: {
+          'string.existingUrl': 'The error message of string.existingUrl',
+        },
+      });
+
+      const schema = custom.object({
+        value1: custom.string().required().existingUrl('add'),
+        value2: custom.string().required().existingUrl('add'),
+        value3: custom.string().required().existingUrl('edit', 'z.example.com'),
+        value4: custom.string().required().existingUrl('edit', 'z.example.com'),
+        value5: custom.string().required().existingUrl('edit', 'z.example.com'),
+      });
+
+      const input = {
+        value1: 'a.example.com',
+        value2: 'b.example.com',
+        value3: 'a.example.com',
+        value4: 'b.example.com',
+        value5: 'z.example.com',
+      };
+
+      const result = schema.validate(input, {
+        abortEarly: false,
+      });
+
+      assert.deepStrictEqual(result.error.details.length, 2);
+      assert.deepStrictEqual(result.error.details[0].message, 'The error message of string.existingUrl');
+      assert.deepStrictEqual(result.error.details[0].path, ['value1']);
+      assert.deepStrictEqual(result.error.details[0].type, 'string.existingUrl');
+      assert.deepStrictEqual(result.error.details[1].message, 'The error message of string.existingUrl');
+      assert.deepStrictEqual(result.error.details[1].path, ['value3']);
+      assert.deepStrictEqual(result.error.details[1].type, 'string.existingUrl');
+    });
   });
 });
