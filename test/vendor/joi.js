dfe2dc0f505c85135b235eacf5db2b5e69639218
smori <shinichiro.mori.1983@gmail.com>
2021-11-03 23:35:07 +0900

    [test] joi - string.hexColor

--- test/vendor/joi.js
+++ test/vendor/joi.js
@@ -4,6 +4,53 @@ const Joi = require('joi');
 
 describe('vendor.joi', () => {
   describe('extend', () => {
+    it('string.hexColor', () => {
+      /**
+       * @type {Joi.Root}
+       */
+      const custom = Joi.extend({
+        type: 'string',
+        rules: {
+          hexColor: {
+            method() {
+              return this.$_addRule({
+                name: 'hexColor',
+              });
+            },
+            validate(value, helpers) {
+              if (/^#[0-9a-f]{6}$/i.test(value) === false) {
+                return helpers.error('string.hexColor');
+              }
+            },
+          },
+        },
+        messages: {
+          'string.hexColor': 'The error message of string.hexColor',
+        },
+      });
+
+      const schema = custom.object({
+        value1: custom.string().required().hexColor(),
+        value2: custom.string().required().hexColor(),
+        value3: custom.string().required().hexColor(),
+      });
+
+      const input = {
+        value1: '#000000',
+        value2: '#ffffff',
+        value3: '#zzzzzz',
+      };
+
+      const result = schema.validate(input, {
+        abortEarly: false,
+      });
+
+      assert.deepStrictEqual(result.error.details.length, 1);
+      assert.deepStrictEqual(result.error.details[0].message, 'The error message of string.hexColor');
+      assert.deepStrictEqual(result.error.details[0].path, ['value3']);
+      assert.deepStrictEqual(result.error.details[0].type, 'string.hexColor');
+    });
+
     it('string.in', () => {
       /**
        * @type {Joi.Root}
