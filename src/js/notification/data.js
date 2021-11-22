3f11c89c397ea7556a080f17c9c37e3961ba5313
smori <shinichiro.mori.1983@gmail.com>
2021-11-23 00:12:11 +0900

    Remove Data.sortByUrl() Data.sortByMessage()

--- src/js/notification/data.js
+++ src/js/notification/data.js
@@ -51,30 +51,6 @@ class Data {
 
     return true;
   }
-
-  /**
-   * @param {PatternItem[]} patterns
-   * @returns {PatternItem[]}
-   */
-  sortByUrl(patterns) {
-    return patterns.sort((a, b) => {
-      return (a.url < b.url) ? -1 : 1;
-    });
-  }
-
-  /**
-   * @param {PatternItem[]} patterns
-   * @returns {PatternItem[]}
-   */
-  sortByMessage(patterns) {
-    return patterns.sort((a, b) => {
-      if (a.msg === b.msg) {
-        return (a.url < b.url) ? -1 : 1;
-      }
-
-      return (a.msg < b.msg) ? -1 : 1;
-    });
-  }
 }
 
 module.exports = Data;
