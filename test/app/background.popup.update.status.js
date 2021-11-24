51bfbae9b86c3f58156ae45af752e31bde1e1a1b
smori <shinichiro.mori.1983@gmail.com>
2021-11-25 01:00:54 +0900

    [test] add tests to background.popup.update.status.js

--- test/app/background.popup.update.status.js
+++ test/app/background.popup.update.status.js
@@ -1,11 +1,19 @@
 const { describe, beforeEach, it } = require('mocha');
 const assert = require('assert');
+const Storage = require('../../src/js/notification/storage');
 const SUT = require('../../src/js/app/background.popup.update.status');
 const testUtil = require('../../test_lib/util');
 
 describe('app.background.popup.update.status', () => {
+  /**
+   * @type {Storage}
+   */
+  let storage;
+
   testUtil.chromeBackground.registerHooks();
   beforeEach(() => {
+    storage = new Storage();
+
     testUtil.initStorage(testUtil.currentVersion().toString(), [
       testUtil.makePatternItem({
         url: 'domain1.example.com',
@@ -30,6 +38,9 @@ describe('app.background.popup.update.status', () => {
     assert.ok(testUtil.chromeBackground.setBadgeTextCalledWith('OFF', 10001));
     assert.strictEqual(data.item.status, 0);
     assert.strictEqual(data.status, 0);
+
+    const pattern = storage.find('domain1.example.com');
+    assert.strictEqual(pattern.status, 0);
   });
 
   it('status is 0, then enable', () => {
@@ -44,6 +55,9 @@ describe('app.background.popup.update.status', () => {
     assert.ok(testUtil.chromeBackground.setBadgeTextCalledWith('ON', 10002));
     assert.strictEqual(data.item.status, 1);
     assert.strictEqual(data.status, 1);
+
+    const pattern = storage.find('domain2.example.com');
+    assert.strictEqual(pattern.status, 1);
   });
 
   it('invoked for non-existing pattern', () => {
@@ -58,5 +72,22 @@ describe('app.background.popup.update.status', () => {
     assert.ok(testUtil.chromeBackground.setBadgeTextCalledWith('', 20001));
     assert.strictEqual(data.item, null);
     assert.strictEqual(data.status, 1);
+  });
+
+  it('sent status is invalid - currently invalid status value will be carried but data will not be updated', () => {
+    const res = testUtil.chromeBackground.sendResponse();
+
+    SUT.listen();
+
+    testUtil.chromeBackground.popupUpdateStatusDispatch(10003, 'domain1.example.com', 9, res.callback);
+
+    /** @type {MessageBrowserActionUpdateStatusResponse} */
+    const data = res.data();
+    assert.ok(testUtil.chromeBackground.setBadgeTextCalledWith('', 10003));
+    assert.strictEqual(data.item.status, 9);
+    assert.strictEqual(data.status, 9);
+
+    const pattern = storage.find('domain1.example.com');
+    assert.strictEqual(pattern.status, 1);
   });
 });
