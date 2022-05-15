fea43cba98eb81350c5639838c35a9711e0fb54d
smori <shinichiro.mori.1983@gmail.com>
2022-05-15 22:58:49 +0900

    Update data migration page

--- src/js/app/manifest.v3.main.js
+++ src/js/app/manifest.v3.main.js
@@ -1,4 +1,11 @@
+require('../../css/options.scss');
+require('../../css/bootstrap-custom.css');
+
 const $ = require('jquery');
+
+require('bootstrap');
+
+const i18n = require('./i18n');
 const v3Util = require('./manifest.v3.util');
 const StorageLegacy = require('../notification/storage-legacy');
 
@@ -8,6 +15,14 @@ const completed = () => {
 };
 
 $(async () => {
+  $('#js_version').text('Ver. ' + chrome.runtime.getManifest().version);
+  i18n.apply($, 'body');
+
+  $('#js_button_options_page').on('click', (e) => {
+    e.preventDefault();
+    location.href = chrome.runtime.getURL('html/options.html');
+  });
+
   if (await v3Util.hasData()) {
     completed();
   } else {
